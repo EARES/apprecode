25dca5cea0a5cd13eacd8e5e571c5e98259976d9
Emre Akbaki <info@emreakbaki.com>
2023-03-21 20:15:40 +0300

    Add layouts and configs, add tailwind package

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,54 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
+  ssr: true,
   target: 'static',
+  generate: {
+    cache: false,
+    fallback: true,
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'apprecode',
+    title: 'AppreCode',
+    titleTemplate: '%s | AppreCode',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'This project prepare for apprecode.',
+      },
       { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'og-type', property: 'og:type', content: 'website' },
+      {
+        hid: 'twitter-description',
+        property: 'twitter:description',
+        content: 'This project prepare for apprecode.',
+      },
+      {
+        hid: 'og-desc',
+        property: 'og:description',
+        content: 'This project prepare for apprecode.',
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content: 'tech, apprecode, caseproject, case',
+      },
+      {
+        hid: 'og-site_name',
+        property: 'og:site_name',
+        content: 'Apprecode',
+      },
+      {
+        hid: 'twitter-card',
+        property: 'twitter:card',
+        content: 'summary',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
@@ -27,7 +63,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: ['@nuxtjs/tailwindcss'],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
